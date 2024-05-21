e757838cf3d92922b71d804aeb6d3063aba086dc
ALAN DE MEDEIROS <alan_de-medeiros@estudante.sesisenai.org.br>
2024-05-21 20:55:04 -0300

    Meu primeiro commit

---
+++ Aula02.js
@@ -0,0 +1,8 @@
+var nome = "Alan de Medeiros"
+var idade = 20
+var eAluno = true 
+var dataNascimento
+
+document.write(typeof nome, '<br/>')
+document.write(typeof dataNascimento, '<br/>')
+document.write(typeof eAluno, 'br/>')
